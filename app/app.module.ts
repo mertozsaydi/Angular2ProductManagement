e16da8d42567f846a21da61f9108f3a115120d1d
Mert Ozsaydi <mertozsaydi@gmail.com>
2017-07-21 14:05:03 -0400

    Implemented custom pipe for filter

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component';
-
+import { ProductFilterPipe }  from './products/product-filter.pipe';
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule
-   ],
+  ],
   declarations: [ 
     AppComponent,
-    ProductListComponent ],
+    ProductListComponent,
+    ProductFilterPipe
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
