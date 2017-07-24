b5d9d9fc05ae7ee47b63dac091fb9efbbc0978c0
Mert Ozsaydi <mertozsaydi@gmail.com>
2017-07-24 08:50:56 -0400

    Added product detail component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+
 import { AppComponent }  from './app.component';
+import { WelcomeComponent }  from './home/welcome.component';
 import { ProductListComponent }  from './products/product-list.component';
+import { ProductDetailComponent }  from './products/product-detail.component';
 import { ProductFilterPipe }  from './products/product-filter.pipe';
 import { StarComponent }  from './shared/star.component';
 
@@ -15,7 +18,9 @@ import { StarComponent }  from './shared/star.component';
   ],
   declarations: [ 
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ProductFilterPipe,
     StarComponent
   ],
