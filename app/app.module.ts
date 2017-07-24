3f44183caaa48ff36a9dbda6517b6bf737d351d9
Mert Ozsaydi <mertozsaydi@gmail.com>
2017-07-24 09:31:42 -0400

    Navigation and routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent }  from './home/welcome.component';
@@ -14,7 +15,14 @@ import { StarComponent }  from './shared/star.component';
   imports: [ 
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'product/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome' , pathMatch : 'full'},
+      { path: '**', redirectTo: 'welcome' , pathMatch : 'full'}
+    ])
   ],
   declarations: [ 
     AppComponent,
