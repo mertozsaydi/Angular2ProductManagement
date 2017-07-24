1e1718828a515c7a4f439296e88bd0eca2e2cc7c
Mert Ozsaydi <mertozsaydi@gmail.com>
2017-07-24 11:32:09 -0400

    Updated product detail page

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Subscription }       from 'rxjs/Subscription';
+
 import {IProduct} from './product';
+import { ProductService } from './product.service';
+
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html',
@@ -10,17 +14,33 @@ import {IProduct} from './product';
 export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product : IProduct;
+    errorMessage: string;
+    private sub: Subscription;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router){
+                private _router: Router,
+                private _productService: ProductService){
 
     }
     ngOnInit(): void {
-        let id = +this._route.snapshot.params['id'];
-        this.pageTitle += `: ${id}`;
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+        });
     }
 
-     onBack(): void {
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
+    }
+    
+    onBack(): void {
         this._router.navigate(['/products']);
     }
 }
