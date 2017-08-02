dbf9ef568f6a9f8ead7081f8c2d1c6eba6d9d12b
Mert Ozsaydi <mertozsaydi@gmail.com>
2017-08-02 11:33:00 -0400

    Added in-memory web api for demos and tests

--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -1,34 +1,43 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+
 import {IProduct} from './product';
 
 @Injectable()
 export class ProductService {
-    private _productUrl = 'api/products/products.json';
+    
+    private _productUrl = 'api/products';
 
     constructor(private _http: Http){}
 
     getProducts(): Observable<IProduct[]> {
         return this._http.get(this._productUrl)
-            .map((response: Response) =><IProduct[]> response.json())
-            .do(data => console.log('All data:' + JSON.stringify(data)))
+            .map(this.extractData)
+            .do(data => console.log('getProducts: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
 
-     getProduct(id: number): Observable<IProduct> {
+    getProduct(id: number): Observable<IProduct> {
         return this.getProducts()
-            .map((products: IProduct[]) => products.find(p => p.productId === id));
+            .map((products: IProduct[]) => products.find(p => p.id === id));
     }
 
 
     private handleError (error: Response) {
         console.log(error);
         return Observable.throw(error.json().error || 'Server error');
+    }
+
+    private extractData(response: Response) {
+        let body = response.json();
+        return body.data || {};
     }
 
 }
